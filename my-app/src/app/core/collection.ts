af6e15c20bd970a70c3511f4024e3ddfd627f019
Catix22 <cathjoyez@yahoo.fr>
2018-07-03 11:33:07 +0200

    ton commenraire

---
+++ my-app/src/app/core/collection.ts
@@ -0,0 +1,23 @@
+import { State } from '../shared/enums/state.enum';
+
+export const COLLECTION:Item[]= [
+  {
+    id: 'a1',
+    name: 'Christ',
+    reference: '12',
+    state: State.ALIVRER
+  },
+  {
+    id: 'b1',
+    name: 'lolo',
+    reference: '1245',
+    state: State.ENCOURS
+  },
+  {
+    id: 'c1',
+    name: 'Bent',
+    reference: '1200',
+    state: State.LIVREE
+  }
+];
+
